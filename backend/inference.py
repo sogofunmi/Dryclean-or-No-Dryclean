a813e3010d836b307e18e83a88a6702a7af0dee5
sogofunmi <debbieogunwale@gmail.com>
2026-08-27 06:39:51 -0400

    .

--- backend/inference.py
+++ backend/inference.py
@@ -1,16 +1,60 @@
 import os
-from sentence_transformers import SentenceTransformer
 import pandas as pd
 from skbio.stats.composition import clr
 
 
+model = None
+scaler = None
+dict_vect = None
+embedding_model = None
+artifacts_loaded = False
 
-def process(response, scaler, dv, emb_model_path):
+def load_artifacts():
+    global model, scaler, dict_vect, artifacts_loaded, embedding_model
+    if _artifacts_loaded:
+        return
+    import mlflow
+    import joblib
+    from sentence_transformers import SentenceTransformer
+    mlflow_uri = os.environ.get("MLFLOW_TRACKING_URI")
+    if not mlflow_uri or "localhost" in mlflow_uri:
+        raise RuntimeError("MLFLOW_TRACKING_URI is unset or pointing to localhost!")
+
+    mlflow.set_tracking_uri(mlflow_uri)
+    model_name = "XGBoost Model"
+    alias = "production"
+
+    client = mlflow.MlflowClient()
+    model_uri = f"models:/{model_name}@{alias}"
+    model = mlflow.xgboost.load_model(model_uri)
+
+    prod_version = client.get_model_version_by_alias(name=model_name, alias=alias)
+    run_id = prod_version.run_id
+
+    scaler_path = mlflow.artifacts.download_artifacts(
+        run_id=run_id, artifact_path="Best_Model/scaler.pkl", dst_path="/tmp"
+    )
+    dict_vect_path = mlflow.artifacts.download_artifacts(
+        run_id=run_id, artifact_path="Best_Model/dict_vect.pkl", dst_path="/tmp"
+    )
+
+    scaler = joblib.load(scaler_path)
+    dict_vect = joblib.load(dict_vect_path)
+    model_path = os.environ.get("MODEL_PATH", "all-MiniLM-L6-v2")
+    embedding_model = SentenceTransformer(model_path)
+    artifacts_loaded = True
+
+
+def process(response):
+
+    load_artifacts()
+    if model is None or scaler is None:
+        return {"error": "Model artifacts failed to load."}
+
     df = pd.json_normalize(response)
     df.columns = df.columns.str.replace("composition.", "", regex=False)
 
     df = df.map(lambda x: x.lower() if isinstance(x, str) else x)
-    embedding_model = SentenceTransformer(emb_model_path)
 
     descr = df["description"].iloc[0]
     emb = embedding_model.encode([descr])
@@ -19,11 +63,11 @@ def process(response, scaler, dv, emb_model_path):
 
     scaled_price = scaler.transform(df[["price"]])
     dicts = df.drop(["description", "price"], axis=1).to_dict(orient="records")
-    fabric = dv.transform(dicts)
+    fabric = dict_vect.transform(dicts)
     fabric = (fabric / 100) + 0.005
     fabric = clr(fabric)
 
-    fab_cols = dv.get_feature_names_out()
+    fab_cols = dict_vect.get_feature_names_out()
     scaled_df = pd.DataFrame(list(fabric), columns=fab_cols)
     scaled_df["price"] = scaled_price
     scaled_df = pd.concat([scaled_df, emb_df], axis=1)
--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ class FabricInput(BaseModel):
     composition: dict[str, int]
 
 
-origins = os.environ.get("ALLOWED_ORIGINS", "http://localhost:5173")
+origins = os.environ.get("ALLOWED_ORIGINS")
 
 app.add_middleware(
     CORSMiddleware,
@@ -25,43 +25,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-model = None
-scaler = None
-dict_vect = None
-emb_model_path = os.environ.get("MODEL_PATH", "all-MiniLM-L6-v2")
-_artifacts_loaded = False
-
-def load_artifacts():
-    global model, scaler, dict_vect, _artifacts_loaded
-    if _artifacts_loaded:
-        return
-
-    mlflow_uri = os.environ.get("MLFLOW_TRACKING_URI")
-    if not mlflow_uri or "localhost" in mlflow_uri:
-        raise RuntimeError("MLFLOW_TRACKING_URI is unset or pointing to localhost!")
-
-    mlflow.set_tracking_uri(mlflow_uri)
-    model_name = "XGBoost Model"
-    alias = "production"
-
-    client = mlflow.MlflowClient()
-    model_uri = f"models:/{model_name}@{alias}"
-    model = mlflow.xgboost.load_model(model_uri)
-
-    prod_version = client.get_model_version_by_alias(name=model_name, alias=alias)
-    run_id = prod_version.run_id
-
-    scaler_path = mlflow.artifacts.download_artifacts(
-        run_id=run_id, artifact_path="Best_Model/scaler.pkl", dst_path="/tmp"
-    )
-    dict_vect_path = mlflow.artifacts.download_artifacts(
-        run_id=run_id, artifact_path="Best_Model/dict_vect.pkl", dst_path="/tmp"
-    )
-
-    scaler = joblib.load(scaler_path)
-    dict_vect = joblib.load(dict_vect_path)
-    _artifacts_loaded = True
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -75,12 +38,8 @@ def health_check():
 def predict(data: FabricInput):
 
     from inference import process
-    load_artifacts()
-    if model is None or scaler is None:
-        return {"error": "Model artifacts failed to load."}
 
-    raw_data = data.model_dump()
-    scaled_df = process(raw_data, scaler, dict_vect, emb_model_path)
+    scaled_df = process(data.model_dump())
     
     prediction = model.predict_proba(scaled_df)
     pos_proba = prediction[0][1]
@@ -92,10 +51,9 @@ def predict(data: FabricInput):
             message = "Don't machine wash!"
     else:
         message = "Error making prediction"        
-    #return {"prediction": message}
-    return JSONResponse(
-        content={"prediction": message},
-        headers={"Access-Control-Allow-Origin": "https://machine-wash-or-not.com"}
-    )
+    return {"prediction": message}
+    #return JSONResponse(
+        #content={"prediction": message},
+        #headers={"Access-Control-Allow-Origin": "https://machine-wash-or-not.com"})
 
 handler = Mangum(app, lifespan="off")
