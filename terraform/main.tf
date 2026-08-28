08e8c290f0b8f7b6561aebdafa323d0ff1d6b851
sogofunmi <debbieogunwale@gmail.com>
2026-08-28 15:29:12 -0400

    Setting up WACL rules for CloudFront

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,6 +63,28 @@ resource "aws_wafv2_web_acl" "cloudfront" {
     default_action {
         allow {}
     }
+
+    rule {
+        name = "rule-1"
+        priority = 1
+
+        action {
+            block {}
+        }
+
+        statement {
+            rate_based_statement {
+                limit = 500
+                aggregate_key_type = "IP"
+            }
+        }
+        visibility_config {
+            cloudwatch_metrics_enabled = false
+            metric_name = "wacl-rate-metric"
+            sampled_requests_enabled = false
+        }
+
+    }   
 }
 
 resource "aws_cloudfront_distribution" "website" {
